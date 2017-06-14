b9c2186a4097edc400888bc91028f7bd51fcf2f2
andrujoh <andru_joh@yahoo.no>
2017-06-14 21:46:59 +0200

    Add project files.

---
+++ Console Kalkulator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Kalkulator
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+    }
+  }
+}
