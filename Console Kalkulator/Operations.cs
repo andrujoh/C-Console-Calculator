4cca2ca935e6b4b74a66677dcb605efa927a42f1
andrujoh <andru_joh@yahoo.no>
2017-09-03 14:39:36 +0200

    Add operations and program logic

---
+++ Console Kalkulator/Operations.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Kalkulator
+{
+  public class Operations
+  {
+    public static double Add(double firstNum, double secNum)
+    {
+      return firstNum + secNum;
+    }
+
+    public static double Subtract(double firstNum, double secNum)
+    {
+      return firstNum - secNum;
+    }
+
+    public static double Multiply(double firstNum, double secNum)
+    {
+      return firstNum * secNum;
+    }
+
+    public static double Divide(double firstNum, double secNum)
+    {
+      // Handle divide by zero
+
+      if (secNum == 0)
+      {
+        Console.WriteLine("\nIkke dele med null vet du!\n");
+        return 0;
+      }
+      else
+      {
+        return firstNum / secNum;
+      }
+
+    }
+
+    public static double Percent(double firstNum, double secNum)
+    {
+      return (secNum * firstNum) / 100;
+    }
+  }
+}
--- Console Kalkulator/Program.cs
+++ Console Kalkulator/Program.cs
@@ -8,8 +8,94 @@ namespace Console_Kalkulator
 {
   class Program
   {
+    // Local variables
+    static double firstNum;
+    static double secNum;
+    static double result;
     static void Main(string[] args)
     {
+      Console.WriteLine("Konsoll Kalkulator");
+
+      // Loop until user quits
+      while (true)
+      {
+        Console.WriteLine("Velg først regnemetode. Etterpå velger du tallene du vil regne med.");
+        Console.WriteLine("Menyvalg");
+        Console.WriteLine("\n1: Addere\n2: Subtrahere\n3: Multiplisere\n4: Dividere\n5: Prosent\n6: Avslutt");
+        var menuInput = Console.ReadLine();
+
+        // Quit
+        if (menuInput == "6")
+        {
+          Console.WriteLine("Ha det bra!");
+          break;
+        }
+
+        // Handle Operations to show result based on menu choice
+        try
+        {
+          int menuVal = int.Parse(menuInput);
+          switch (menuVal)
+          {
+            case 1:
+              GetNumbers();
+              result = Operations.Add(firstNum, secNum);
+              break;
+            case 2:
+              GetNumbers();
+              result = Operations.Subtract(firstNum, secNum);
+              break;
+            case 3:
+              GetNumbers();
+              result = Operations.Multiply(firstNum, secNum);
+              break;
+            case 4:
+              GetNumbers();
+              result = Operations.Divide(firstNum, secNum);
+              break;
+            case 5:
+              GetNumbers();
+              result = Operations.Percent(firstNum, secNum);
+              break;
+            default:
+              Console.WriteLine("\nVelg et tall mellom 1 og 6\n");
+              continue;
+          }
+
+          // Show calculation result
+          Console.WriteLine($"\n********** Resultatet er {result} **********\n");
+        }
+
+        // Handles users being derpy
+        catch (FormatException)
+        {
+          Console.WriteLine("\nVelg en av regnemetodene i menyen.\n");
+        }
+      }
+    }
+
+    // Get the numbers to calculate
+    static private void GetNumbers()
+    {
+      // Loop until both inputs are valid
+      while (true)
+      {
+        try
+        {
+          Console.Write("Skriv inn det første tallet: ");
+          firstNum = double.Parse(Console.ReadLine());
+          Console.Write("Skriv inn det andre tallet: ");
+          secNum = double.Parse(Console.ReadLine());
+          break;
+        }
+
+        // Derpy user protection 
+        catch (FormatException)
+        {
+          Console.WriteLine("Skriv inn et gyldig tall");
+          continue;
+        }
+      }
     }
   }
 }
